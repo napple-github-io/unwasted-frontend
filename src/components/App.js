9584012fd732f4f92bda3304b7d9907362340366
Payton Lower <paytown@me.com>
2019-06-16 14:44:55 -0700

    user profile display component renders

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './display/main.css';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import Home from './display/Home';
-import YourProfile from './display/YourProfile';
-
+// import YourProfile from './display/YourProfile';
+import UserProfile from './display/UserProfile';
 
 export default function App() {
   return (
-    <YourProfile />
+    <UserProfile />
   );
 }
--- src/components/profile/UserProfileDetails.js
+++ src/components/profile/UserProfileDetails.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function UserProfile({ userProfile }) {
+function UserProfileDetails({ userProfileDetails }) {
   return (
     <section>
-      <img src={userProfile.image} alt={userProfile.userName} />
+      <img src={userProfileDetails.userImage} alt={userProfileDetails.username} />
       <h5>Name</h5>
-      <h4>{userProfile.name}</h4>
+      <h4>{userProfileDetails.firstName} {userProfileDetails.lastName}</h4>
       <h5>Location</h5>
-      <h4>{userProfile.city}, {userProfile.state}</h4>
+      <h4>{userProfileDetails.location.street}, {userProfileDetails.location.state}</h4>
       <h5>Standing</h5>
-      <h4>{userProfile.standing}</h4>
+      <h4>{userProfileDetails.standing}</h4>
       <button>CONTACT</button>
     </section>
   );
 }
 
-UserProfile.propTypes = {
-  userProfile: PropTypes.object.isRequired
+UserProfileDetails.propTypes = {
+  userProfileDetails: PropTypes.object.isRequired
 };
 
-export default UserProfile;
+export default UserProfileDetails;
