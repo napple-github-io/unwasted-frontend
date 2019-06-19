35a65f5ba83e162ae0671b58166179f54cd4ff24
Payton Lower <paytown@me.com>
2019-06-19 11:37:37 -0700

    signup form styled

--- src/containers/auth/SignUpSubmit.js
+++ src/containers/auth/SignUpSubmit.js
@@ -50,6 +50,7 @@ export default class SignUpSubmit extends PureComponent {
             city={city}
             onSubmit={this.onSubmit}
             onChange={this.onChange}
+            className={styles.forms}
           />
         </main>
 
