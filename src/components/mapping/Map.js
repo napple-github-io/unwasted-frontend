6089c716c9b891b000a208fdce048b39b8deecbb
Sean Nelson <smnelsonn@gmail.com>
2019-10-11 14:06:25 -0700

    drawing map with default props

--- src/components/mapping/Map.js
+++ src/components/mapping/Map.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Map.css';
 import { Map as GoogleMap, GoogleApiWrapper } from 'google-maps-react';
 
-function Map({ mapUrl }) {
-  return (
-    <>
-      <img className={styles.map} src={mapUrl || `https://maps.googleapis.com/maps/api/staticmap?center=Portland,OR&key=${process.env.MAPS_API_KEY}&size=980x980`} />
-    </>
-  );
+export class MapContainer extends Component{
+  render() {
+    return (
+      <>
+        {/* <img className={styles.map} src={mapUrl || `https://maps.googleapis.com/maps/api/staticmap?center=Portland,OR&key=${process.env.MAPS_API_KEY}&size=980x980`} /> */}
+        <GoogleMap
+          google={this.props.google}
+          zoom={12}
+          style={styles}
+          initialCenter={ { lat: 45.5051, lng: -122.55 } }
+        />
+      </>
+    );
+  }
+
 }
 
-Map.propTypes = {
-  mapUrl: PropTypes.string.isRequired
+MapContainer.propTypes = {
+  google: PropTypes.object.isRequired
 };
 
-export default Map;
-// export default GoogleApiWrapper({
-//  apiKey: process.env.MAPS_API_KEY
-// })(MapContainer);
+// export default Map;
+export default GoogleApiWrapper({
+  apiKey: process.env.MAPS_API_KEY
+})(MapContainer);
